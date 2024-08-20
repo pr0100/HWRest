c8e0cac86f0205b38ec0ee439060597c3d866e49
pr0100 <ssstrelkov2000@gmail.com>
2024-08-20 11:56:19 +0500

    Добавил новый тест и изменил существующий

--- src/test/java/tests/Profile/ProfileTest.java
+++ src/test/java/tests/Profile/ProfileTest.java
@@ -2,36 +2,37 @@ package tests.Profile;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.confirm;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static specs.BooksSpec.addBookRequestSpec;
 import static specs.BooksSpec.booksResponseSpec;
 import static tests.TestData.defaultLogin;
 import static tests.TestData.elemArrayBooks;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.response.Response;
 import com.codeborne.selenide.Configuration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import models.IsbnModel;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.openqa.selenium.Cookie;
 import models.AddBookRequestModel;
-import models.AddBookResponseModel;
 import org.junit.jupiter.api.Test;
 import tests.API.ApiConfig;
 import tests.API.AuthorizationApi;
 
-public class LoginTest extends ApiConfig {
+@DisplayName("Тесты для вкладки Profile")
+public class ProfileTest extends ApiConfig {
 
   AuthorizationApi authorizationApi = new AuthorizationApi();
 
   @Test
+  @DisplayName("Успешная авторизация")
   void successfulLoginApi() {
     String authCookieTokenKey = "token";
     String authCookieExpiresKey = "expires";
@@ -63,13 +64,41 @@ public class LoginTest extends ApiConfig {
   }
 
   @Test
-  void addBooks() {
+  @DisplayName("Удаление всех книг у пользователя")
+  void deleteAllUsersBooks() {
+    Response responseUser = authorizationApi.getLoginResponse();
+    String authCookieUserIDValue = responseUser.path("userId");
+
+    Response responseToken = authorizationApi.getTokenResponse();
+    String authCookieTokenValue = responseToken.path("token");
+
+    step("Make request" , () ->
+        given()
+            .filter(new AllureRestAssured())
+            .auth().basic(responseUser.path("username"), responseUser.path("password"))
+            .auth().oauth2(authCookieTokenValue)
+            .log().all()
+        .when()
+            .delete("BookStore/v1/Books?UserId=" + authCookieUserIDValue)
+        .then()
+            .log().all()
+            .statusCode(204));
+  }
+
+  @Test
+  @DisplayName("Добавление книги через API и удаление через UI")
+  void addBookToUserApiAndDeleteUI() {
+    String authCookieTokenKey = "token";
+    String authCookieExpiresKey = "expires";
+    String authCookieUserIDKey = "userID";
+
     AddBookRequestModel regParams = new AddBookRequestModel();
     Response responseUser = authorizationApi.getLoginResponse();
     String authCookieUserIDValue = responseUser.path("userId");
 
     Response responseToken = authorizationApi.getTokenResponse();
     String authCookieTokenValue = responseToken.path("token");
+    String authCookieExpiresValue = responseToken.path("expires");
 
     step("Get random available Isbn", () -> {
       Response responseAllIsbn = get("BookStore/v1/Books");
@@ -81,7 +110,7 @@ public class LoginTest extends ApiConfig {
       regParams.setCollectionOfIsbns(isbnList);
     });
 
-    AddBookResponseModel response = step("Make request" , () ->
+    step("Add book to user", () ->
         given(addBookRequestSpec)
             .auth().basic(responseUser.path("username"), responseUser.path("password"))
             .auth().oauth2(authCookieTokenValue)
@@ -89,10 +118,28 @@ public class LoginTest extends ApiConfig {
         .when()
             .post()
         .then()
-            .spec(booksResponseSpec)
-            .extract().as(AddBookResponseModel.class));
+            .spec(booksResponseSpec));
+
+    step("Open an authorized page using cookies", () -> {
+      Configuration.pageLoadStrategy = "eager";
+      open("https://demoqa.com/profile");
+      Cookie authCookieToken = new Cookie(authCookieTokenKey, authCookieTokenValue);
+      Cookie authCookieExpires = new Cookie(authCookieExpiresKey, authCookieExpiresValue);
+      Cookie authCookieUserID = new Cookie(authCookieUserIDKey, authCookieUserIDValue);
+
+      getWebDriver().manage().addCookie(authCookieToken);
+      getWebDriver().manage().addCookie(authCookieExpires);
+      getWebDriver().manage().addCookie(authCookieUserID);
+      getWebDriver().navigate().refresh();
+    });
+
+    step("Delete book using UI", () -> {
+      $("#delete-record-undefined").click();
+      $("#closeSmallModal-ok").click();
+      confirm();
+    });
 
     step("Check result", () ->
-      assertEquals(regParams.getCollectionOfIsbns(), response.getBooks()));
+        $(".rt-noData").shouldHave(text("No rows found")));
   }
 }
